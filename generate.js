2b0cca2a394cb8bdb1c3a913bf07ce370c88160b
Spencer Smith <dontaskspencer@gmail.com>
2019-06-13 10:44:23 -0600

    Update generate.js

--- generate.js
+++ generate.js
@@ -1,4 +1,5 @@
-/*
+(()=>{
+  /*
  * Edit: Link for index page found in dars report.
  * Generate and view report and click on approved class links below some sections.
  *
@@ -6,18 +7,22 @@
  * ("Search for a Class" link in CIS)
  */
 
-var c = document.querySelectorAll('td .PAGROUPBOXLABELLEVEL1.PSLEFTCORNER');
-var larry = document.createElement('div');
-for(var i = 0; i < c.length; i++){
-  var parts = c[i].innerText.trim().split(' ');
-  var dep = parts[0];
-  var course = parts[1];
-  var title = c[i].innerText.trim().split('-')[1].trim();
-  var a = document.createElement('a');
-  var li = document.createElement('li');
-  a.innerText = c[i].innerText.trim();
-  a.href = "https://dars.sys.utah.edu/selfservice/plannedcourse/preview.html?department=" + dep + "&number=" + course;
-  li.appendChild(a);
-  larry.appendChild(li);
-}
-document.body.appendChild(larry);
+  const classes = document.querySelectorAll('td .PAGROUPBOXLABELLEVEL1');
+  const output = [];
+
+  classes.forEach(c => {
+    const text = c.innerText.trim();
+    const [dep, course] = text.split(' ');
+    const title = text.split('-')[1].trim();
+    const a = document.createElement('a');
+    const li = document.createElement('li');
+    a.innerText = text;
+    a.href = "https://dars.sys.utah.edu/selfservice/plannedcourse/preview.html?department=" + dep + "&number=" + course;
+    a.setAttribute('target', 'view');
+    li.appendChild(a);
+    output.push(li.outerHTML);
+  });
+
+  console.log(output.join('\n'));
+  
+})();
