2dc9e8f7c9197b1ea22a2b0068f5264a890ca7cb
Spencer Smith <ssmith@sofi.org>
2020-10-27 14:53:26 -0600

    Nicer format

--- generate.js
+++ generate.js
@@ -11,30 +11,34 @@
   const output = [];
 
   classes.forEach(c => {
-    const fullName = c.innerText.trim();
-    const [dep, course] = fullName.split(' ');
+    const parts = c.innerText.trim().split(' - ');
+    const [dep, course] = parts[0].split(' ');
+    const name = parts[1];
 
     if (parseInt(course) >= 6000) return;
+    const url = "https://dars.sys.utah.edu/selfservice/plannedcourse/preview.html?department=" + dep + "%20%20%20&number=" + course;
 
     const parent = c.parentNode.parentNode.parentNode;
     const infoCells = parent.querySelectorAll('tr[onclick] td');
-    const times = infoCells[2].innerText;
     const professor = infoCells[4].innerText.replace(/[\n]/g, ' ').toLowerCase();
+    const rawTime = infoCells[2].innerText;
+    const hours = (rawTime.match(/\w (\d.*$)/) || [])[1] || '';
 
     const li = document.createElement('li');
-    const a = document.createElement('a');
-
-    a.innerHTML = [
-      `<div class="name">${fullName}</div>`,
+    li.innerHTML = [
+      `<div class="name">${dep} ${course} - ${name} <a href="${url}" target="view">info</a></div>`,
       '<div class="bottom">',
-      `<div class="times">${times}</div>`,
-      `<div class="professor">${professor}</div>`,
+        `<div class="professor">${professor}</div>`,
+        `<div class="times">`,
+          `<span class="day ${rawTime.includes('Mo') ? 'yes' : ''}">Mo</span>`,
+          `<span class="day ${rawTime.includes('Tu') ? 'yes' : ''}">Tu</span>`,
+          `<span class="day ${rawTime.includes('We') ? 'yes' : ''}">We</span>`,
+          `<span class="day ${rawTime.includes('Th') ? 'yes' : ''}">Th</span>`,
+          `<span class="day ${rawTime.includes('Fr') ? 'yes' : ''}">Fr</span>`,
+          `<span class="hours">${hours}</span>`,
+        '</div>',
       '</div>',
     ].join('');
-    a.href = "https://dars.sys.utah.edu/selfservice/plannedcourse/preview.html?department=" + dep + "%20%20%20&number=" + course;
-    a.setAttribute('target', 'view');
-
-    li.appendChild(a);
 
     output.push(li.outerHTML);
   });
